46b0c4a24805128d6c5fddc335ece66d58810f96
Mark <mark.tabletpc@gmail.com>
2025-08-09 17:34:42 +0200

    feat: add rtk api

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { rtkPokemonApi } from './rtkPokemonApi';
 import selectedItemsReducer from './slices/selectedItemsSlice';
 
 export const store = configureStore({
   reducer: {
     selectedItems: selectedItemsReducer,
+    [rtkPokemonApi.reducerPath]: rtkPokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(rtkPokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
